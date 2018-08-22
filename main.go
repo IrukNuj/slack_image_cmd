9e64159245a8ac37b178eae1447d8e4f8af6dec0
ozin <b1506231@planet.kanazawa-it.ac.jp>
2018-08-23 02:29:16 +0900

    setup GAE

--- main.go
+++ main.go
@@ -1,43 +1,46 @@
-package main
+package slack_image_cmd
 
 import (
 	"net/http"
-	"github.com/nlopes/slack"
 	"fmt"
-	"encoding/json"
-	"github.com/JinOketani/slack_image_cmd/lib"
+	"github.com/nlopes/slack"
 	"os"
+	"encoding/json"
+	"google.golang.org/appengine"
+	"github.com/JinOketani/slack_image_cmd/lib"
 )
 
-func main() {
-	http.HandleFunc("/cmd", func(w http.ResponseWriter, r *http.Request) {
-		s, err := slack.SlashCommandParse(r)
+func init() {
+	http.HandleFunc("/cmd", handler)
+	appengine.Main()
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hi!")
+	s, err := slack.SlashCommandParse(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if !s.ValidateToken(os.Getenv("VERIFICATION_TOKEN")) {
+		fmt.Println(s)
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	switch s.Command {
+	case "/image":
+		response := &slack.Msg{Text: lib.SearchImage(s.Text), ResponseType: "in_channel"}
+		image, err := json.Marshal(response)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-
-		if !s.ValidateToken(os.Getenv("VERIFICATION_TOKEN")) {
-			fmt.Println(s)
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-
-		switch s.Command {
-		case "/image":
-			response := &slack.Msg{Text: lib.SearchImage(s.Text), ResponseType: "in_channel"}
-			image, err := json.Marshal(response)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			w.Header().Set("Content-Type", "application/json")
-			w.Write(image)
-		default:
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-	})
-	fmt.Println("[Info] Server start")
-	http.ListenAndServe(os.Getenv("PORT"), nil)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(image)
+	default:
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
